22e14c16a6c9c41fe4ca891224839d4b1b506b10
Lee <lqw676869@gmail.com>
2016-08-16 18:56:04 +0800

    新增数据库帮助类，以及个表对应的实体类

---
+++ src/main/java/com/lee/coolweather/model/Province.java
@@ -0,0 +1,34 @@
+package com.lee.coolweather.model;
+
+/**
+ * Created by Lee on 2016/8/16.
+ */
+public class Province {
+    private int id;
+    private String provinceName;
+    private String provinceCode;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+
+    public String getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setProvinceCode(String provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+}
